7655d47020202e51d318084ba870dc92004ebce3
BaronNox <baron.von.stabossen@gmail.com>
2017-05-09 03:45:19 +0200

    impl: OOB/Occupied

--- TTD-L/src/main/java/ch03TicTacToe/TicTacToe.java
+++ TTD-L/src/main/java/ch03TicTacToe/TicTacToe.java
@@ -2,5 +2,27 @@ package ch03TicTacToe;
 
 public class TicTacToe
 {
-
+	private Character[][] board = { {'\0', '\0', '\0'},
+									{'\0', '\0', '\0'},
+									{'\0', '\0', '\0'} };
+	
+	public void play(int x, int y) {
+		checkAxis(x);
+		checkAxis(y);
+		setBox(x, y);
+	}
+	
+	private void checkAxis(int v) {
+		if(v < 1 || v > 3) {
+			throw new RuntimeException("X is outside board.");
+		}
+	}
+	
+	private void setBox(int x, int y) {
+		if(board[x - 1][y - 1] != '\0') {
+			throw new RuntimeException("Box is occupied.");
+		} else {
+			board[x - 1][y - 1] = 'X';
+		}
+	}
 }
